17b0ed3de7989657ed03f57639bc5c3d36a6e93b
JD <jdmejia712@gmail.com>
2021-10-27 19:20:14 -0400

    Add files via upload
    completed

--- index.js
+++ index.js
@@ -1,29 +1,91 @@
 // Your code here
 let dodger = document.getElementById('dodger')
+//------------Horizontal Function-----------//
 dodger.style.backgroundColor = '#FF69B4';
-document.addEventListener('keydown', function(e){
-    if(e.key==='ArrowLeft'){
-        left = parseInt(dodger.style.left)
-        movedLeft = left - (50)
+function moveDodgerLeft (){
+    left = parseInt(dodger.style.left)
+    movedLeft = left - (5)
+    if(left>0){
         dodger.style.left = movedLeft+'px'
         console.log(dodger.style.left)
     }
-    if(e.key==='ArrowRight'){
-        left = parseInt(dodger.style.left)
-        movedRight = left + (50)
+}
+
+function moveDodgerRight (){
+    left = parseInt(dodger.style.left)
+    movedRight = left + (5)
+    if(left<380){
         dodger.style.left = movedRight+'px'
         console.log(dodger.style.left)
     }
-    if(e.key==='ArrowDown'){
-        bottom = parseInt(dodger.style.bottom)
-        movedDown = bottom - (50)
+}
+//------------Horizontal Function-----------//
+
+//------------Vertical Function-----------//
+function moveDodgerDown (){
+    bottom = parseInt(dodger.style.bottom)
+    movedDown = bottom - (5)
+    if(bottom>0){
         dodger.style.bottom = movedDown+'px'
         console.log(dodger.style.bottom)
     }
-    if(e.key==='ArrowUp'){
-        bottom = parseInt(dodger.style.bottom)
-        movedUp = bottom + (50)
-        dodger.style.bottom = movedUp+'px'
+}
+
+function moveDodgerUp (){
+    bottom = parseInt(dodger.style.bottom)
+    movedDown = bottom + (5)
+    if(bottom<380){
+        dodger.style.bottom = movedDown+'px'
         console.log(dodger.style.bottom)
     }
-})
+}
+//------------Vertical Function-----------//
+
+
+
+//------------Horizontal-----------//
+document.addEventListener('keydown', function(e){
+    if(e.key==='ArrowLeft'){
+        moveDodgerLeft();
+}})
+
+document.addEventListener('keydown', function(e){
+    if(e.key==='ArrowRight'){
+        moveDodgerRight();
+}})
+
+//------------Vertical-----------//
+document.addEventListener('keydown', function(e){
+    if(e.key==='ArrowDown'){
+        moveDodgerDown();
+}})
+
+document.addEventListener('keydown', function(e){
+    if(e.key==='ArrowUp'){
+        moveDodgerUp();
+}})
+
+
+// left = parseInt(dodger.style.left)
+// movedLeft = left - (1)
+// dodger.style.left = movedLeft+'px'
+// console.log(dodger.style.left)
+// }
+// if(e.key==='ArrowRight'){
+// left = parseInt(dodger.style.left)
+// movedRight = left + (1)
+// dodger.style.left = movedRight+'px'
+// console.log(dodger.style.left)
+// }
+// if(e.key==='ArrowDown'){
+// bottom = parseInt(dodger.style.bottom)
+// movedDown = bottom - (1)
+// dodger.style.bottom = movedDown+'px'
+// console.log(dodger.style.bottom)
+// }
+// if(e.key==='ArrowUp'){
+// bottom = parseInt(dodger.style.bottom)
+// movedUp = bottom + (1)
+// dodger.style.bottom = movedUp+'px'
+// console.log(dodger.style.bottom)
+// }}
